9e7024feff8245c17c6b968ce2cfa3b7ff8b73c6
fuzzshums <ryanhiggins444@gmail.com>
2018-04-15 22:41:18 -0400

    Revert "Revert "added powerup""
    This reverts commit acc41c17ecd6bb25cefd89f02c7f23edeead058c.

---
+++ SpikeCena/Assets/Powerup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour {
+
+    Vector2 randomPos;
+    Random rnd = new Random();
+
+	// Use this for initialization
+	void Start () {
+        randomizePos();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void randomizePos()
+    {
+        randomPos = new Vector2(Random.Range(-8.0f, 6.0f) ,-4.3f);
+        this.transform.position = randomPos;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.GetComponent<Player>().changeBullet();
+            randomizePos();
+        }
+    }
+}
